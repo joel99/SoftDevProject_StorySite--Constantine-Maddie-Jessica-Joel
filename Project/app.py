ec15b65015f61cc84fa345385ef77ff6c75e9860
Maddie Ostergaard <Mostergaard@stuy.edu>
2016-11-01 00:18:29 -0400

    started form for new post

--- Project/app.py
+++ Project/app.py
@@ -4,21 +4,21 @@ import sqlite3
 app = Flask(__name__)
 app.secret_key = "secrets"
 
-@app.route("/")
+@app.route("/") #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def root():
     if "user" in session:#if logged in
         return render_template('home.html', usr = session["user"])
     else:#if not logged in
         return render_template('login.html')
 
-@app.route("/login")
+@app.route("/login") #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def login():
     d = request.form
     if isValidLogin(d["username"], d["pass"]):
         session["user"] = d["username"]
-    return redirect(url_for('home'))
+    return redirect(url_for('root'))
 
-@app.route("/register")
+@app.route("/register") #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def register():
     d = request.form
     if isValidRegister(d["pass1"], d["pass2"], d["username"]):
@@ -27,7 +27,15 @@ def register():
         session["user"] = d["userName"]
     return redirect(url_for('home'))
 
-
+@app.route('/<string:username>') #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def userPage(username):
+    d = request.form
+    if "newPost" in request.args:
+        # CODE TO PUT FORM INFO INTO DB
+        # CODE TO DISPLAY POST
+        return render_template('userPage.html', username = username)
+    else:
+        return render_template('userPage.html', username = username)
 
 if __name__ == "__main__":
     app.debug = True
