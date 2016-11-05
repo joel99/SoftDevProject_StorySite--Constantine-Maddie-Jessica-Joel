958b25115c98366535a28ed3f2b19cf7c44a4166
Joel <jye5@stuy.edu>
2016-11-05 12:13:01 -0400

    Stories now displayed on homepage. Refactored more database utils, cleared some database content. Not functional until blob changed to text. Updated app.py, add todo

--- Project/utils/storyUtil.py
+++ Project/utils/storyUtil.py
@@ -7,12 +7,9 @@ def getStoryIDs(userID):
 def getStoryUpdate(storyID):
     return storyDBUtil.getStoryUpdateInfo(storyID)
 
-<<<<<<< HEAD
 def getStory(storyID):
-	return
+	return 0
 
-def 
-=======
 def randStoryID():
     return storyDBUtil.randStoryID()
->>>>>>> a4879583223701dbb59f60bf496960842cf914bc
+
